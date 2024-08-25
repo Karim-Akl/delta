7cd531802314f162767f23a91674168c5a838cb5
Karim Akl <karimaql30@gmail.com>
2024-08-25 10:56:00 +0300

    karimk

--- src/app/rent-countries/page.jsx
+++ src/app/rent-countries/page.jsx
@@ -1,99 +1,52 @@
-import React from "react";
-import Link from "next/link";
-import Contact from "../../components/contact/page";
+import React from 'react';
 import Header from "../../components/header/page";
 import Footer from "../../components/footer/page";
-import Image from "next/image";
-import "../../components/hero/hero.css";
-import "./rent-countries.css";
+import './rent-countries.css';
+import countriesData from '../../data/countries.json';
+import countriesData2 from '../../data/countries-2.json';
+import Link from "next/link";
+
+const CountryList = ({ title, description, countries }) => (
+  <>
+    <div className='bg-count'>
+      <div className='count-class'>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+
+    <div className='bg-count-class'>
+      <div className='count'>
+        {countries.map((country) => (
+          <div key={country.id}>
+            <img src={country.image} alt={country.name} />
+            <p>{country.name}</p>
+            <button><Link href={`/country-details/${country.id}`}>المزيد</Link></button>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  </>
+);
 
 const Page = () => {
-  const handleContactClick = () => {
-    console.log("Contact button clicked");
-    // Add your desired behavior here
-  };
-
   return (
-    <section className="sec-serv">
+    <section>
       <Header />
-      <div className="backServices">
-        <h3>شركة دلتا النسيم</h3>
-        <p>خدمات معتمدة</p>
-      </div>
-
-      <div className="flex line-3-bg">
-        <div className="imga" style={{ width: "50%" }}>
-          <h2>نحن نقدم عروض مغرية</h2>
-          <p>
-            تقديم أسعار تنافسية مع الحفاظ على جودة الخدمة، مما يجعل خدماتنا في
-            متناول الجميع. شفافية الأسعار: وضوح وتفصيل الأسعار بدون أي رسوم خفية،
-            مما يعزز من ثقة العملاء. سلامة وأمان
-          </p>
-          <button><Link href="/our-services"></Link>توصل معنا</button>
-        </div>
-        <div className="imga" style={{ width: "30%" }}>
-          <Image
-            src="/image/pexels-tima-miroshnichenko-6200780.jpg"
-            alt="عروض مغرية"
-            width={200}
-            height={200}
-            priority
-          />
-        </div>
-      </div>
-
-      <div className="class-bg">
-        <div className="service-container">
-          <h2>خدماتنا</h2>
-          <p>خبراء في مجالنا لذلك يمكننا توفير خدمات مميزة لك</p>
-          <div className="flex">
-            {[
-              {
-                title: "إيجار العاملات",
-                description:
-                  "تقديم خدمات إيجار العمالة المنزلية مع مرونة في العقود والأسعار التنافسية تأمين العمالة المنزلية من البلدان المصرح بها، مع ضمان التزامهم بأعلى معايير الجودة",
-                imgSrc: "/image/0821202411020566c5c92d7be51.gif",
-                altText: "إيجار العاملات",
-              },
-              {
-                title: "تنظيف شامل",
-                description:
-                  "تنظيف المنازل والمكاتب بأساليب احترافية لضمان بيئة نظيفة وصحية",
-                imgSrc: "/image/0821202411031766c5c975cc7b2.gif",
-                altText: "تنظيف شامل",
-              },
-              {
-                title: "رعاية منزلية",
-                description:
-                  "توفير الرعاية للمسنين والأطفال بأعلى مستوى من الاهتمام والعناية. نهدف إلى جعل حياتكم أسهل وأكثر راحة من خلال خدماتنا المتكاملة والمخصصة لتلبية جميع احتياجاتكم المنزلية.",
-                imgSrc: "/image/0821202411045566c5c9d744f0e.gif",
-                altText: "رعاية منزلية",
-              },
-            ].map(({ title, description, imgSrc, altText }, index) => (
-              <div key={index} className="l1-class-bg">
-                <div className="service-text">
-                  <h2>{title}</h2>
-                  <p>{description}</p>
-                </div>
-                <div className="service-image">
-                  <Image
-                    src={imgSrc}
-                    alt={altText}
-                    width={250}
-                    height={250}
-                    priority
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      <Contact />
+      <CountryList
+        title="دول الايجار"
+        description="تعرف علي كافه التفاصيل"
+        countries={countriesData}
+      />
+      <CountryList
+        title="دول الاستقدام"
+        description="تعرف علي كافه التفاصيل"
+        countries={countriesData2}
+      />
       <Footer />
     </section>
   );
-};
+}
 
 export default Page;
--- src/components/hero/page.jsx
+++ src/components/hero/page.jsx
@@ -4,208 +4,64 @@ import Link from "next/link";
 import "./hero.css";
 
 const Hero = () => {
-  const [count, setCount] = useState(0);
+  const [recruitmentCount, setRecruitmentCount] = useState(0);
+  const [rentalCount, setRentalCount] = useState(0);
 
-  // Increment the count from 0 to 8 with a smooth animation
+  // Increment the count for recruitment countries from 0 to 20
   useEffect(() => {
-    let startCount = 0;
-    const endCount = 20;
+    const startCount = 0;
+    const endCountRecruitment = 20;
+    const endCountRental = 8;
     const duration = 3000;
-    const incrementTime = duration / (endCount - startCount);
+
+    const incrementTimeRecruitment = duration / (endCountRecruitment - startCount);
+    const incrementTimeRental = duration / (endCountRental - startCount);
   
-    const incrementCount = () => {
-      setCount((prevCount) => {
-        if (prevCount < endCount) {
-          setTimeout(incrementCount, incrementTime);
+    const incrementRecruitmentCount = () => {
+      setRecruitmentCount((prevCount) => {
+        if (prevCount < endCountRecruitment) {
+          setTimeout(incrementRecruitmentCount, incrementTimeRecruitment);
           return prevCount + 1;
         } else {
-          return endCount;
+          return endCountRecruitment;
+        }
+      });
+    };
+
+    const incrementRentalCount = () => {
+      setRentalCount((prevCount) => {
+        if (prevCount < endCountRental) {
+          setTimeout(incrementRentalCount, incrementTimeRental);
+          return prevCount + 1;
+        } else {
+          return endCountRental;
         }
       });
     };
   
-    incrementCount();
+    incrementRecruitmentCount();
+    incrementRentalCount();
   
   }, []);
 
-  // Render a box with a dynamic count
-  const renderBox = () => (
+  // Render a box with a dynamic count and title
+  const renderBox = (count, title, link) => (
     <div className="box">
       <h2>{count}</h2>
-      <p>دول استقدام</p>
-      <button><Link href="/rent-countries">المزيد</Link></button>
+      <p>{title}</p>
+      <button><Link href={link}>المزيد</Link></button>
     </div>
   );
 
   return (
     <section>
       <div className="hero-class">
-        <div className="flex img-text-hero">
-          <div>
-            <img
-              src="/image/0821202410584166c5c861b7d60.gif"
-              alt="Hero"
-              width={400}
-            />
-          </div>
-          <div className="text">
-            <p>تعرف علينا أكثر</p>
-            <h3>من نحن</h3>
-            <p>
-              شركة دلتا النسيم للاستقدام شركة سعودية مساهمة مغلقة (مقفلة) تأسست
-              في عام 2021 م في استقدام العمالة المنزلية في منطقة القصيم بريدة
-              بترخيص رقم 74 وقد بدأت بتقديم خدمة الوساطة لاستقدام العمالة
-              المنزلية وتسهيل إجراءات الاستقدام ما بين العملاء ووكالات العمل
-              الخارجية بإضافة الى هذه الخدمة نقوم بخدمة نقل الخدمات من الشركة
-              الى العميل مباشرة ونعمل على تلبية احتياج قطاع الافراد في استقدام
-              العمالة المنزلية للعديد من الجنسيات ومختلف المهن لكلا الجنسين وفق
-              ما ورد في وزارة العمل والتنمية الاجتماعية ونظل دائما على العمل
-              والانجاز السريع وتقديم افضل الخدمات في مجال استقدام العمالة
-              والتأجير.
-            </p>
-          </div>
-        </div>
-        <div className="flex l3-hero">
-          <div className="l1-hero">
-            <div>
-              <img src="/image/l1.svg" alt="Vision" style={{    background: "#092143",padding: "20px",  borderRadius: "61px"}}/>
-            </div>
-            <h2>رؤيتنا</h2>
-            <p>
-              نسعى في شركة دلتا النسيم للاستقدام لأن نكون الشريك الاستراتيجي
-              المفضل لتوفير الكوادر البشرية ذات الكفاءة العالية في المملكة
-              العربية السعودية.
-            </p>
-            <hr className="l3-hr" />
-          </div>
-
-          <div className="l1-hero">
-            <div>
-              <img src="/image/l2.svg" alt="Mission"style={{    background: "#092143",padding: "20px",  borderRadius: "61px"}} />
-            </div>
-            <h2>مهمتنا</h2>
-            <p>
-              أن نكون الشركة الرائدة في تقديم خدمات العمالة المنزلية من خلال
-              التميز في الأداء وتقديم أعلى مستويات الخدمة. تلبية احتياجات
-              العملاء: فهم احتياجات عملائنا وتقديم حلول مخصصة تلبي توقعاتهم
-
-
-            </p>
-            <hr className="l2-hr" />
-          </div>
-
-          <div className="l1-hero">
-            <div>
-              <img src="/image/l3.svg" alt="Professionalism" style={{    background: "#092143",padding: "20px",  borderRadius: "61px"}}/>
-            </div>
-            <h2>احترافية الأداء</h2>
-            <p>
-              ضمان تقديم خدمات منزلية متميزة تلبي أعلى معايير الجودة
-              والاحترافية. رضا العملاء: التركيز على تحقيق رضا العملاء الكامل من
-              خلال تلبية احتياجاتهم وتقديم نتائج تفوق توقعاتهم. توفير كوادر
-              بشرية ذات كفاءة عالية.
-            </p>
-            <hr />
-          </div>
-        </div>
-      </div>
-
-      <div className="class-bg">
-        <div style={{ width: "80%", margin: "auto", gap: "0px" }}>
-          <div
-            style={{
-              width: "100%",
-              marginTop: "5rem",
-              justifyContent: "center",
-              color: "white",
-            }}
-          >
-            <h2>خدماتنا</h2>
-            <p>خبراء في مجالنا، لذلك يمكننا توفير خدمات مميزة لك</p>
-          </div>
-          <div className="flex">
-            <div className="l1-class-bg " style={{ height: "25rem", gap: "1rem" }}>
-              <p>
-                تقديم خدمات إيجار العمالة المنزلية مع مرونة في العقود والأسعار
-                التنافسية. تأمين العمالة المنزلية من البلدان المصرح بها، مع
-                ضمان التزامهم بأعلى معايير الجودة.
-              </p>
-              <h2>إيجار العاملات</h2>
-              <img src="/image/0821202411045566c5c9d744f0e.gif" alt="Worker rental" width={250} />
-            </div>
-
-            <div className="l1-class-bg" style={{ height: "25rem", gap: "1rem" }}>
-              <p
-              >تنظيف المنازل والمكاتب بأساليب احترافية لضمان بيئة نظيفة وصحية
-              والمخصصة لتلبية جميع احتياجاتكم المنزلية.</p>
-              <h2>تنظيف شامل</h2>
-              <img src="/image/0821202411031766c5c975cc7b2.gif" alt="Comprehensive cleaning" width={250} />
-            </div>
-
-            <div className="l1-class-bg" style={{ height: "25rem", gap: "1rem" }}>
-              <p>
-                توفير الرعاية للمسنين والأطفال بأعلى مستوى من الاهتمام والعناية.
-                نهدف إلى جعل حياتكم أسهل وأكثر راحة من خلال خدماتنا المتكاملة
-                والمخصصة لتلبية جميع احتياجاتكم المنزلية.
-              </p>
-              <h2>رعاية منزلية</h2>
-              <img src="/image/0821202411020566c5c92d7be51.gif" alt="Home care" width={250} />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex line-3-bg">
-        <div style={{ width: "50%" }} className="imga">
-          <h2>نحن نقدم عروض مغرية</h2>
-          <p>
-            تقديم أسعار تنافسية مع الحفاظ على جودة الخدمة، مما يجعل خدماتنا في
-            متناول الجميع. شفافية الأسعار: وضوح وتفصيل الأسعار بدون أي رسوم
-            خفية، مما يعزز من ثقة العملاء. سلامة وأمان.
-          </p>
-          <Link href="/contact">
-            <button>تواصل معنا</button>
-          </Link>
-        </div>
-        <div style={{ width: "30%" }} className="imga">
-          <img
-            src="/image/pexels-tima-miroshnichenko-6200780.jpg"
-            alt="Offers"
-            width={200}
-          />
-        </div>
-      </div>
-
-      <div className="flex line-5-bg">
-        <div>
-          <img src="/image/logo.png" alt="Logo" />
-        </div>
-        <div>
-          <p>
-            نسعى في شركة أيادي النهار لأن نكون الشريك الاستراتيجي المفضل لتوفير
-            الكوادر البشرية ذات الكفاءة العالية في المملكة العربية السعودية.
-          </p>
-          <h3>تحقيق التميز</h3>
-          <p>
-            ✔ أن نكون الشركة الرائدة في تقديم خدمات العمالة المنزلية من خلال
-            التميز في الأداء وتقديم أعلى مستويات الخدمة.
-          </p>
-          <h3>رضا العملاء</h3>
-          <p>
-            ✔ التركيز على تحقيق رضا العملاء الكامل من خلال تلبية احتياجاتهم
-            وتقديم نتائج تفوق توقعاتهم.
-          </p>
-          <h3>انتقاء دقيق</h3>
-          <p>
-            ✔ استقدام وتوظيف العمالة المنزلية ذات المهارات والخبرات العالية
-            لضمان تقديم أفضل الخدمات.
-          </p>
-        </div>
+        {/* Your other hero content here */}
       </div>
 
       <div className="flex bg-box">
-        {renderBox()}
-        {renderBox()}
+        {renderBox(recruitmentCount, "دول الاستقدام", "/rent-countries")}
+        {renderBox(rentalCount, "دول الايجار", "/rent-countries")}
       </div>
     </section>
   );
